f58e2d09345481440b22276b50389da0069b7917
Pierre-Avr <lansepierre@gmail.com>
2020-11-30 17:03:46 +0100

    update interface

--- InterfaceProjetBDD/InterfaceProjetBDD/MyFormCommandes.h
+++ InterfaceProjetBDD/InterfaceProjetBDD/MyFormCommandes.h
@@ -34,7 +34,7 @@ namespace InterfaceProjetBDD {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Button^ button1;
+
 	protected:
 
 	private:
@@ -50,24 +50,13 @@ namespace InterfaceProjetBDD {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
-			// 
-			// button1
-			// 
-			this->button1->Location = System::Drawing::Point(245, 107);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(379, 178);
-			this->button1->TabIndex = 0;
-			this->button1->Text = L"button1";
-			this->button1->UseVisualStyleBackColor = true;
 			// 
 			// MyFormCommandes
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(936, 447);
-			this->Controls->Add(this->button1);
+			this->ClientSize = System::Drawing::Size(1486, 653);
 			this->Name = L"MyFormCommandes";
 			this->Text = L"MyFormCommandes";
 			this->Load += gcnew System::EventHandler(this, &MyFormCommandes::MyFormCommandes_Load);
